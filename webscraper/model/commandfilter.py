b1a6b19ba155c53807b6689350db9c63f3f9b43c
jaredsethnz <jared.gombert@gmail.com>
2016-09-15 15:38:06 +1200

    smell-three-step-7
    Created a new class called CommandFilter which will contain all the
    functions related to command argument filtering from OptionFilter
    class. I created a constructor in this class which accepts a View
    object and stores a reference to it. I also copied over any constants
    which were being used by these functions from OptionFilter.

---
+++ webscraper/model/commandfilter.py
@@ -0,0 +1,87 @@
+
+
+class CommandFilter(object):
+
+    COMMAND_OPTION = 0
+    COMMAND_PARAM = 1
+    COMMAND_PARAM_2 = 2
+    COMMAND_PARAM_3 = 3
+    COMMAND_COUNT = 1
+    COMMAND_COUNT_1 = 1
+    COMMAND_COUNT_2 = 2
+    COMMAND_COUNT_3 = 3
+    PARAMETER_ONE = 0
+    PARAMETER_TWO = 1
+    PARAMETER_THREE = 2
+    COMMAND_PARAM_DELIMITER = '--'
+    COMMAND_SECOND_LEVEL_DELIMITER = '|'
+    COMMAND_SECOND_LEVEL_DELIMITER_TWO = ':'
+    COMMAND_ERROR_MSG = 'command error, run help command' \
+                        ' for command details.....'
+
+    def __init__(self, console_view):
+        self.view = console_view
+
+    def command(self, args, options):
+        commands = self.check_args(args, options)
+        if commands is not None:
+            for command in commands:
+                command[self.COMMAND_OPTION](*command[1:])
+        else:
+            return self.COMMAND_ERROR_MSG
+
+    def method_options(self, attr, options):
+        option = options.get(attr)
+        if option is not None:
+            return getattr(self, option)
+        else:
+            return None
+
+    def check_args(self, args, options):
+        commands = []
+        valid_commands = True
+        for arg in args:
+            command = []
+            arg_param = arg.split(self.COMMAND_PARAM_DELIMITER)
+            param = options.get(arg_param[self.COMMAND_OPTION])
+            if param is not None and len(arg_param) == \
+                    param[self.COMMAND_COUNT]:
+                method = getattr(self, param[self.COMMAND_OPTION])
+                command.append(method)
+                value1 = arg_param[self.COMMAND_PARAM] if \
+                    param[self.COMMAND_COUNT] > self.COMMAND_COUNT_1 else None
+                value2 = arg_param[self.COMMAND_PARAM_2] if \
+                    param[self.COMMAND_COUNT] > self.COMMAND_COUNT_2 else None
+                value3 = arg_param[self.COMMAND_PARAM_3] if \
+                    param[self.COMMAND_COUNT] > self.COMMAND_COUNT_3 else None
+                command.append(value1)
+                command.append(value2)
+                command.append(value3)
+            else:
+                valid_commands = False
+            commands.append(command)
+        return None if not valid_commands or len(args) == 0 else commands
+
+    def check_second_level_args(self, args):
+        params = []
+        pattern = re.compile('[A-Za-z0-9]+:[A-Za-z0-9]+:[A-Za-z0-9]+$')
+        args = args[self.COMMAND_OPTION].split(self
+                                               .COMMAND_SECOND_LEVEL_DELIMITER)
+        for arg in args:
+            if pattern.match(arg):
+                param = arg.split(self.COMMAND_SECOND_LEVEL_DELIMITER_TWO)
+                params.append([param[self.PARAMETER_ONE], param[self.
+                              PARAMETER_TWO], param[self.PARAMETER_THREE]])
+            else:
+                self.view.display_item(self.COMMAND_ERROR_MSG)
+                return None
+                break
+        return None if len(params) is 0 else params
+
+    def check_second_level_param_count(self, params, count):
+        if len(params) == count:
+            return True
+        else:
+            self.view.display_item('incorrect parameter count, expected ' +
+                                   str(count) + '.....')
+            return False
